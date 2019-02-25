849cedaa6b70e6b32825b8a798bdbd75a656a332
gzbenxx <gzbenxx@gmail.com>
2019-02-25 09:57:36 +0800

    build topic page

---
+++ learning_logs/urls.py
@@ -0,0 +1,16 @@
+"""define URL patterns for learning_logs"""
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+
+    #home page
+    url(r'^$',views.index , name='index'),
+
+    #show all topics
+    url(r'^topics/$', views.topics, name='topics'),
+
+    #detail page for a single topic
+    url(r'^topics/(?P<topic_id>\d+)/$', views.topic, name='topic'),
+
+]

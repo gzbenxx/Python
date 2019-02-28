8b47d31828560833201c8ae5231b9c3f9ca649b6
gzbenxx <gzbenxx@gmail.com>
2019-02-28 17:12:13 +0800

    2019-2-28 after work

--- users/urls.py
+++ users/urls.py
@@ -1,11 +1,16 @@
 """defines URL patterns for users """
 
 from django.conf.urls import url
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView,LogoutView
+
 
 from . import views
 
 urlpatterns = [
     # login page
     url(r'^login/$',LoginView.as_view(template_name = 'users\login.html'),name="login"),
+    #logout 
+    url(r'^logout/$',LogoutView.as_view(),name='logout'),
+    #registration 
+    url(r'^register/$', views.register, name='register'),
 ]
